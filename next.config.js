b8b83e2337cfa98e54f5815705fe3603ea89f334
Jack SZE TO <fhy26548726@gmail.com>
2022-03-12 18:21:09 +0800

    init project structure

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  styledComponent: true,
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  }
 }
 
 module.exports = nextConfig
