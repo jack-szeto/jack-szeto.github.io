0ff25c886a8a8dbdc8c9e3bdcfc8e3b1fafcf546
Jack SZE TO <fhy26548726@gmail.com>
2022-04-03 19:14:35 +0800

    disable static image

--- next.config.js
+++ next.config.js
@@ -1,29 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  styledComponent: true,
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
-  },
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.(ogg|mp3|wav|mpe?g)$/i,
-      use: {
-        loader: 'file-loader',
-        options: {
-          publicPath: '/_next/static/sounds/',
-          outputPath: 'static/sounds/',
-          name: '[name].[ext]',
-          esModule: false,
-        },
-      },
-    });
+    reactStrictMode: true,
+    styledComponent: true,
+    pwa: {
+        dest: "public",
+        register: true,
+        skipWaiting: true,
+        disable: process.env.NODE_ENV === "development"
+    },
+    images: {
+        disableStaticImages: true
+    },
+    webpack: (config, options) => {
+        config.module.rules.push({
+            test: /\.(ogg|mp3|wav|mpe?g)$/i,
+            use: {
+                loader: 'file-loader',
+                options: {
+                    publicPath: '/_next/static/sounds/',
+                    outputPath: 'static/sounds/',
+                    name: '[name].[ext]',
+                    esModule: false
+                }
+            }
+        });
 
-    return config
-  },
+        return config
+    }
 }
 
 module.exports = nextConfig
